8eba53d1804e91085212f10ddfac4c772b5343d1
Shahriar Shojib <shahriar_shojib@hotmail.com>
2022-02-27 13:16:28 +0600

    updated types update hooks import to compat

--- src/useStore.ts
+++ src/useStore.ts
@@ -1,5 +1,5 @@
 import { ReturnStoreType, StateConstraint } from '@poly-state/poly-state';
-import { useEffect, useState } from 'preact/hooks';
+import { useEffect, useState } from 'preact/compat';
 
 export const useStore = <T extends StateConstraint>(store: ReturnStoreType<T>) => {
 	const [state, setState] = useState(store.getState());
